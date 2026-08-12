14c6e0708983ccc64be697d6491df220a7c5cc75
Bella Richmond <bella.richmond@proton.me>
2026-08-12 11:12:52 -0400

    assign site numbers (unconfirmed)

--- R/calc_bird_div.R
+++ R/calc_bird_div.R
@@ -1,20 +1,19 @@
 calc_bird_div <- function(birds_raw) {
   # collapse distance categories
   b_s <- birds_raw %>%
-    select(c(Transect, Species, X0_25, X25_50, X50_)) %>%
-    drop_na() %>%
-    mutate(X25_ = X25_50 + X50_) %>%
-    select(-c(X25_50, X50_))
+    rowwise() %>%
+  	mutate(X25. = sum(X25.50, X50., na.rm = T)) %>%
+    select(c(Transect, Species, X0.25, X25.))
 
   # calculate diversity metrics
   b_close <- b_s %>%
-    select(c(Transect, Species, X0_25)) %>%
+    select(c(Transect, Species, X0.25)) %>%
     group_by(Transect, Species) %>%
-    summarize(X0_25 = sum(X0_25)) %>%
+    summarize(X0.25 = sum(X0.25)) %>%
     pivot_wider(
       id_cols = Transect,
       names_from = Species,
-      values_from = X0_25
+      values_from = X0.25
     ) %>%
     replace(is.na(.), 0) %>%
     column_to_rownames('Transect') %>%
@@ -28,13 +27,13 @@ calc_bird_div <- function(birds_raw) {
     rownames_to_column('Transect')
 
   b_far <- b_s %>%
-    select(c(Transect, Species, X25_)) %>%
+    select(c(Transect, Species, X25.)) %>%
     group_by(Transect, Species) %>%
-    summarize(X25_ = sum(X25_)) %>%
+    summarize(X25. = sum(X25.)) %>%
     pivot_wider(
       id_cols = Transect,
       names_from = Species,
-      values_from = X25_
+      values_from = X25.
     ) %>%
     replace(is.na(.), 0) %>%
     column_to_rownames('Transect') %>%
@@ -69,5 +68,21 @@ calc_bird_div <- function(birds_raw) {
       names_to = 'dist',
       values_to = 'val'
     ) %>%
-    separate_wider_delim(dist, names = c('div', 'dist'), delim = '_')
+    separate_wider_delim(dist, names = c('div', 'dist'), delim = '_') %>%
+  	mutate(site = case_when(site == "ARBO-01" ~ "ARBO_S1",
+  													site == "ARBO-02" ~ "ARBO_L1",
+  													site == "ARBO-03" ~ "ARBO_L2",
+  													site == "ARBO-04" ~ "ARBO_S2",
+  													site == "BDL-1" ~ "BDL_S2",
+  													site == "BDL-2" ~ "BDL_L1",
+  													site == "BDL-3" ~ "BDL_S1",
+  													site == "BDL-4"~ "BDL_L2",
+  													site == "STNY-01" ~ "STNY_L1",
+  													site == "STNY-02" ~ "STNY_L2",
+  													site == "STNY-03" ~ "STNY_S1",
+  													site == "STNY-04" ~ "STNY_S2",
+  													site == "TECHNO-1" ~ "TECHNO_L1",
+  													site == "TECHNO-2" ~ "TECHNO_S1",
+  													site == "TECHNO-3" ~ "TECHNO_L2",
+  													site == "TECHNO-4" ~ "TECHNO_S2"))
 }
